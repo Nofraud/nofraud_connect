31faa97ecc1f3b7542c2b13cdf442e68dc5a4bbf
Soleil Cotterell <s.cotterell@nofraud.com>
2025-01-07 10:14:11 -0600

    fix: Attempt to void the payment if refund fails

---
+++ Exception/InvoiceRefundOrVoidException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace NoFraud\Connect\Exception;
+
+/**
+ * Thrown if an invoice cannot be refunded or voided
+ */
+class InvoiceRefundOrVoidException extends \Exception
+{
+}
---
+++ Exception/InvoiceVoidException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace NoFraud\Connect\Exception;
+
+/**
+ * Thrown when invoice cannot be voided
+ */
+class InvoiceVoidException extends \Exception
+{
+}
